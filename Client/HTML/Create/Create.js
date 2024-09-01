f2a7dbe6925e0b15ceae4159fde5a8d65fed7844
Andres <andresmaldonado200338@gmail.com>
2024-08-31 22:14:26 -0500

    Creación de buses

---
+++ Client/HTML/Create/Create.js
@@ -0,0 +1,53 @@
+document.getElementById('newBusForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const plateLetters = document.getElementById('floatingInputPlateLetters');
+    const plateNumbers = document.getElementById('floatingInputPlateNumbers');
+    const arrivalTime = document.getElementById('floatingInputArrivalTime');
+
+    const totalLetters = /^[A-Z]{3}$/;
+    const totalNumbers = /^\d{3}$/;
+
+    plateLetters.classList.remove('is-invalid');
+    plateNumbers.classList.remove('is-invalid');
+    arrivalTime.classList.remove('is-invalid');
+
+    let isValid = true;
+
+    if (!totalLetters.test(plateLetters.value.toUpperCase())) {
+        plateLetters.classList.add('is-invalid');
+        isValid = false;
+    }
+
+    if (!totalNumbers.test(plateNumbers.value)) {
+        plateNumbers.classList.add('is-invalid');
+        isValid = false;
+    }
+
+    if (!arrivalTime.value) {
+        arrivalTime.classList.add('is-invalid');
+        isValid = false;
+    }
+
+    if (isValid) {
+        const busInfo = {
+            bus_plate: plateLetters.value.toUpperCase() + plateNumbers.value,
+            bus_last_arrival: arrivalTime.value
+        };
+
+        fetch('http://localhost:3000/buses', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(busInfo)
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert(`Transmilenio con matrícula ${data.bus_plate} creado con éxito.`);
+        })
+        .catch(error => {
+            alert('Error al crear el bus: ' + error.message);
+        });
+    }
+});
---
+++ Client/JS/header.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Carga el header en cada página
+    fetch('/Client/HTML/Header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+
+            // De la URL solo es toma el que vaya despues del "/"
+            const path = window.location.pathname.split('/').pop();
+
+            // Se elimina el active en todas las clases
+            document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+                link.classList.remove('active');
+            });
+
+            // Se verifica la ruta actual y aplica la clase "active" al enlace correspondiente
+            const navLinks = {
+                'Index.html': 'nav-home',
+                'create.html': 'nav-create',
+                'edit.html': 'nav-edit',
+                'show_all.html': 'nav-show',
+                'search.html': 'nav-search',
+                'delete.html': 'nav-delete'
+            };
+
+            const activeLinkId = navLinks[path];
+            if (activeLinkId) {
+                const activeLink = document.getElementById(activeLinkId);
+                if (activeLink) {
+                    activeLink.classList.add('active');
+                }
+            }
+        })
+        .catch(error => console.error('Error al cargar el header:', error));
+});
